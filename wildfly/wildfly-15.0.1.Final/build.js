56da35ed76ea247273c211f10114e1091d81d1e4
Michael Bornholdt Nielsen <michaelbornholdtnielsen@gmail.com>
2019-01-22 14:05:12 +0100

    Update java and add Wildfly-15.0.1.Final

--- wildfly/wildfly-15.0.1.Final/build.js
+++ wildfly/wildfly-15.0.1.Final/build.js
@@ -1,4 +1,4 @@
 #!/usr/bin/jjs -fv
-var cmd = "docker build -t jarrydk/java:11 ."
+var cmd = "docker build -t jarrydk/wildfly:15.0.1.Final ."
 var System = Java.type("java.lang.System");
 $EXEC(cmd, System.in, System.out, System.err);
